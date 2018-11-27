bb90a71dc1626b3af396faad3ee59fd6b7390a73
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2018-11-27 18:40:46 +0100

    fix gdb settings

--- gdb-settings.el
+++ gdb-settings.el
@@ -3,23 +3,23 @@
 ;; Add color to the current GUD line (obrigado google)
 ;; ===================================================================
 
-(defvar gud-overlay
-  (let* ((ov (make-overlay (point-min) (point-min))))
-    (overlay-put ov 'face 'secondary-selection)
-    ov)
-  "Overlay variable for GUD highlighting.")
+;; (defvar gud-overlay
+;;   (let* ((ov (make-overlay (point-min) (point-min))))
+;;     (overlay-put ov 'face 'secondary-selection)
+;;     ov)
+;;   "Overlay variable for GUD highlighting.")
 
-(defadvice gud-display-line (after my-gud-highlight act)
-           "Highlight current line."
-           (let* ((ov gud-overlay)
-                  (bf (gud-find-file true-file)))
-             (with-current-buffer (move-overlay ov (line-beginning-position) (line-end-position)))))
+;; (defadvice gud-display-line (after my-gud-highlight act)
+;;            "Highlight current line."
+;;            (let* ((ov gud-overlay)
+;;                   (bf (gud-find-file true-file)))
+;;              (with-current-buffer (move-overlay ov (line-beginning-position) (line-end-position)))))
 
-(defun gud-kill-buffer ()
-  (if (eq major-mode 'gud-mode)
-    (delete-overlay gud-overlay)))
+;; (defun gud-kill-buffer ()
+;;   (if (eq major-mode 'gud-mode)
+;;     (delete-overlay gud-overlay)))
 
-(add-hook 'kill-buffer-hook 'gud-kill-buffer)
+;; (add-hook 'kill-buffer-hook 'gud-kill-buffer)
 
 
 ;; =================================
@@ -37,14 +37,12 @@
 
 (defun gdb-setup-windows ()
   "Define layout for gdb-many-windows"
-  (gdb-display-locals-buffer)
-  (delete-other-windows)
-  (gdb-display-stack-buffer)
-  (delete-other-windows)
-  (gdb-display-breakpoints-buffer)
-  (delete-other-windows)
-  ;; Don't dedicate.
+  (gdb-get-buffer-create 'gdb-locals-buffer)
+  (gdb-get-buffer-create 'gdb-stack-buffer)
+  (gdb-get-buffer-create 'gdb-breakpoints-buffer)
+  (set-window-dedicated-p (selected-window) nil)
   (switch-to-buffer gud-comint-buffer)
+  (delete-other-windows)
   (let ((win0 (selected-window))
         (win1 (split-window nil ( / ( * (window-height) 3) 4)))
         (win2 (split-window nil ( / (window-height) 3)))
@@ -67,5 +65,26 @@
                                (gdb-breakpoints-buffer-name))
                              nil win5))
     (select-window win0)))
+
+
+;; =======================================
+;; Restore window layout after closing gdb
+;; =======================================
+
+;; Select a register number which is unlikely to get used elsewere
+(defconst before-gdb-windows-config-register 313465989
+   "Internal used")
+
+(defvar before-gdb-windows-config nil)
+
+(defun set-before-gdb-windows-config (gdb)
+  (setq before-gdb-windows-config (window-configuration-to-register before-gdb-windows-config-register)))
+
+(defun restore-before-gdb-windows-config ()
+  (jump-to-register before-gdb-windows-config-register))
+
+(advice-add 'gdb :before #'set-before-gdb-windows-config)
+(advice-add 'gdb-reset :after #'restore-before-gdb-windows-config)
+
 
 (provide 'gdb-settings)

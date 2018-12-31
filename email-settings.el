e3ce74ac1febb80923d5b6f47f97065a71f8db92
Christopher Lackner <christopher.lackner@gmx.at>
2018-12-31 13:31:51 +0100

    fix mu4e settings, build installs now

--- email-settings.el
+++ email-settings.el
@@ -15,10 +15,6 @@
 (setq mu4e-contexts
       `(,(make-mu4e-context
           :name "gmx"
-          :match-func (lambda (msg)
-                        (when msg
-                          (mu4e-message-contact-field-matches msg
-                                                              :to "christopher.lackner@gmx.at")))
           :vars '((user-mail-address ."christopher.lackner@gmx.at")
                   (user-full-name ."Christopher Lackner")
                   (mu4e-maildir ."~/mail/gmx")
@@ -26,13 +22,16 @@
                   (mu4e-drafts-folder ."/Drafts")
                   (mu4e-trash-folder ."/Trash")
                   (mu4e-refile-folder ."/Archives")
-                  )),
-        (make-mu4e-context
+                  (smtpmail-smtp-user ."christopher.lackner@gmx.at")
+                  (smtpmail-smtp-server ."mail.gmx.net")
+                  (smtpmail-smtp-service ."587")
+                  )
+          :match-func (lambda (msg)
+                        (when msg
+                          (string= (mu4e-message-field msg :maildir) "~/mail/gmx")))
+          )
+        ,(make-mu4e-context
          :name "tu"
-         :match-func (lambda (msg)
-                       (when msg
-                         (mu4e-message-contact-field-matches msg
-                                                             :to "christopher.lackner@tuwien.ac.at")))
          :vars '((user-mail-address ."christopher.lackner@tuwien.ac.at")
                  (user-full-name ."Christopher Lackner")
                  (mu4e-maildir ."~/mail/tu")
@@ -40,7 +39,15 @@
                  (mu4e-drafts-folder ."/Drafts")
                  (mu4e-trash-folder ."/Trash")
                  (mu4e-refile-folder ."/Archives")
-                 ))
+                 (smtpmail-smtp-user ."clackner")
+                 (smtpmail-smtp-mail-address ."christopher.lackner@tuwien.ac.at")
+                 (smtpmail-smtp-server ."mail.intern.tuwien.ac.at")
+                 (smtpmail-smtp-service ."587")
+                 )
+          :match-func (lambda (msg)
+                        (when msg
+                          (string= (mu4e-message-field msg :maildir) "~/mail/tu")))
+          )
         ))
 
 (setq message-kill-buffer-on-exit t)
@@ -57,27 +64,29 @@
      (smtpmail-smtp-server "mail.intern.tuwien.ac.at"))
     ))
 
-(defun my-mu4e-set-account ()
-  "Set the account for composing a message.
-   This function is taken from:
-     https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
-  (let* ((account
-    (if mu4e-compose-parent-message
-        (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-    (string-match "/\\(.*?\\)/" maildir)
-    (match-string 1 maildir))
-      (completing-read (format "Compose with account: (%s) "
-             (mapconcat #'(lambda (var) (car var))
-            my-mu4e-account-alist "/"))
-           (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-           nil t nil nil (caar my-mu4e-account-alist))))
-   (account-vars (cdr (assoc account my-mu4e-account-alist))))
-    (if account-vars
-  (mapc #'(lambda (var)
-      (set (car var) (cadr var)))
-        account-vars)
-      (error "No email account found"))))
-(add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
+(setq message-send-mail-function 'smtpmail-send-it)
+
+;; (defun my-mu4e-set-account ()
+;;   "Set the account for composing a message.
+;;    This function is taken from:
+;;      https://www.djcbsoftware.nl/code/mu/mu4e/Multiple-accounts.html"
+;;   (let* ((account
+;;     (if mu4e-compose-parent-message
+;;         (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
+;;     (string-match "/\\(.*?\\)/" maildir)
+;;     (match-string 1 maildir))
+;;       (completing-read (format "Compose with account: (%s) "
+;;              (mapconcat #'(lambda (var) (car var))
+;;             my-mu4e-account-alist "/"))
+;;            (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
+;;            nil t nil nil (caar my-mu4e-account-alist))))
+;;    (account-vars (cdr (assoc account my-mu4e-account-alist))))
+;;     (if account-vars
+;;   (mapc #'(lambda (var)
+;;       (set (car var) (cadr var)))
+;;         account-vars)
+;;       (error "No email account found"))))
+;; (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
 
 (require 'mu4e-alert)
 

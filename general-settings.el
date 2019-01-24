e6cf6366b5154fe3f9ece024cdacd15e20ac9fdb
Christopher Lackner <christopher.lackner@tuwien.ac.at>
2019-01-24 13:23:02 +0100

    fix path from shell

--- general-settings.el
+++ general-settings.el
@@ -12,11 +12,12 @@
 ; set PATH, because we don't load .bashrc
 ; function from https://gist.github.com/jakemcc/3887459
 (defun set-exec-env-vars()
-  (dolist (var (list "PATH" "PYTHONPATH" "MKLROOT" "MKL_THREADING_LAYER"))
-    (setenv var (concat "/usr/local/bin:" (getenv var)))
-    (let ((path-from-shell (shell-command-to-string "$SHELL -c 'echo -n $PATH'")))
-      (setenv var path-from-shell)
-      (setq exec-path (split-string path-from-shell path-separator)))))
+  (dolist (var (list "PATH" "PYTHONPATH" "LD_LIBRARY_PATH" "MKLROOT" "MKL_THREADING_LAYER"))
+    ;; (setenv var (getenv var))
+    (let ((env-from-shell (shell-command-to-string (concat "/bin/bash -i -c 'echo -n $" var "' 2> /dev/null"))
+                          ))
+      (setenv var env-from-shell)
+      )))
 
 (if window-system (set-exec-env-vars))
 

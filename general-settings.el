3dc3b3dcc3d7399d9be74b5e70d64493f6472973
Christopher Lackner <clackner@cerbsim.com>
2019-10-06 21:27:16 +0200

    add smex

--- general-settings.el
+++ general-settings.el
@@ -129,4 +129,13 @@
                     :weight 'normal
                     :width 'normal)
 
+(use-package smex
+  :ensure t)
+(smex-initialize)
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
 (provide 'general-settings)

638ace8bf684cff2957f1846c01e0ed797af240d
Christopher Lackner <clackner@cerbsim.com>
2020-05-13 15:07:18 +0200

    add function to add screenshot in org mode buffer

--- org-mode-settings.el
+++ org-mode-settings.el
@@ -78,4 +78,18 @@
 
 (custom-set-variables '(org-trello-files '("~/org/trello/ngs.org" "")))
 
+(defun my-org-screenshot ()
+  "Take a screenshot into a time stamped unique-named file in the
+same directory as the org-buffer and insert a link to this file."
+  (interactive)
+  (setq filename
+        (concat
+         (make-temp-name
+          (concat (buffer-file-name)
+                  "_"
+                  (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+  (call-process "import" nil nil nil filename)
+  (insert (concat "[[" filename "]]"))
+  (org-display-inline-images))
+
 (provide 'org-mode-settings)

36b0435e41d56d70548953e5bb85d94d0a4c5c71
Christopher Lackner <clackner@cerbsim.com>
2020-06-22 22:23:03 +0200

    add default include file that includes all other files

---
+++ my-settings.el
@@ -0,0 +1,28 @@
+
+(require 'general-settings)
+
+(require 'popup)
+
+(require 'org-mode-settings)
+
+(require 'magit-settings)
+
+(require 'autocomplete-settings)
+
+(require 'cpp-settings)
+
+(require 'python-settings)
+
+(require 'ngsolve-settings)
+
+(require 'rst-settings)
+
+(require 'ein-settings)
+
+(require 'glsl-settings)
+
+(require 'latex-settings)
+
+(require 'gdb-settings)
+
+(provide 'my-settings)

cf58abca0d0a2f772e6d1ae2f19f8a5300afb606
Christopher Lackner <clackner@cerbsim.com>
2023-04-07 17:44:45 +0200

    more gpt stuff

--- gpt-settings.el
+++ gpt-settings.el
@@ -31,20 +31,61 @@
     (start-chatbot (concat "--mode assistant --message \""
                            (concat (replace-tics-in-string message) "\n" reg) "\""))))
 
-(defun start-coder (message)
+(defun ask-coder (message)
+  "Starts a coder bot session and ask what to do with it"
+  (interactive "sEnter your message: ")
+  (start-chatbot (concat "--model gpt-3.5-turbo --message \""
+                           (concat (replace-tics-in-string message)) "\"")))
+
+(defun ask-coder-on-region (message)
+  "Starts a coder bot session and ask what to do with it with the current region as the context"
+  (interactive "sEnter your message: ")
+  (let ((reg (get-formatted-buffer-region)))
+    (start-chatbot (concat "--model gpt-3.5-turbo  --message \""
+                           (concat (replace-tics-in-string message) "\n" reg) "\""))))
+
+(defun ask-coder-4 (message)
   "Starts a coder bot session and ask what to do with it"
   (interactive "sEnter your message: ")
   (start-chatbot (concat "--message \""
                            (concat (replace-tics-in-string message)) "\"")))
 
-(defun start-coder-on-region (message)
+(defun ask-coder-4-on-region (message)
   "Starts a coder bot session and ask what to do with it with the current region as the context"
   (interactive "sEnter your message: ")
   (let ((reg (get-formatted-buffer-region)))
-    (start-chatbot (concat " --message \""
+    (start-chatbot (concat "--message \""
                            (concat (replace-tics-in-string message) "\n" reg) "\""))))
 
+(global-set-key (kbd "C-c C-o a") 'ask-coder)
+(global-set-key (kbd "C-c C-o r") 'ask-coder-on-region)
 
-(global-set-key (kbd "C-c C-o d") 'chatbot-create-docstring)
+
+(defun ask-simpl-chatbot (message)
+  "Starts a simpl chatbot session with as message"
+  (interactive "sEnter your message: ")
+  (start-chatbot (concat "--mode assistant --memory --memory_file simpl --message \""
+                           (concat (replace-tics-in-string message)) "\"")))
+
+(defun ask-simpl-chatbot-region (message)
+  "Starts a simpl chatbot session with as message"
+  (interactive "sEnter your message: ")
+  (let ((reg (get-formatted-buffer-region)))
+    (start-chatbot (concat "--mode assistant --memory --memory_file simpl --message \""
+                           (concat (replace-tics-in-string message) "\n" reg) "\""))))
+
+(defun train-simpl-chatbot (message)
+  "Starts a simpl chatbot session with as message"
+  (interactive "sEnter your message: ")
+  (start-chatbot (concat "--mode assistant --memory --train --memory_file simpl --message \""
+                           (concat (replace-tics-in-string message)) "\"")))
+
+(defun train-simpl-chatbot-region (message)
+  "Starts a simpl chatbot session with as message"
+  (interactive "sEnter your message: ")
+  (let ((reg (get-formatted-buffer-region)))
+    (start-chatbot (concat "--mode assistant --memory --train --memory_file simpl --message \""
+                           (concat (replace-tics-in-string message) "\n" reg) "\""))))
+
 
 (provide 'gpt-settings)
